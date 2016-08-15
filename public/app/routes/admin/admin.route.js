79da8e54c32caadc8db337bb1851f60491ad4a28
Tate Price <priceat@gmail.com>
2016-08-15 11:23:47 -0500

    Update admin.route.js

--- public/app/routes/admin/admin.route.js
+++ public/app/routes/admin/admin.route.js
@@ -1,12 +1,14 @@
 import {Route} from 'app/modules/router/route';
 import {Controller} from './admin.ctrl';
 import template from './admin.html!text';
+import { default as servicesModuleName } from './company.services';
+
 
 // If your route has any Angular module dependencies,
 // import them into this file and list them out in
 // the ngRouteDependencies array;
 
-var ngRouteDependencies = [];
+var ngRouteDependencies = [servicesModuleName];
 
 // Create and export a new Route instance. The options
 // passed here are used to define a ui-router state.
