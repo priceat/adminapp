1baed1ca769212f82ff01f7a4ea3d0d23b5f0284
Tate Price <priceat@gmail.com>
2016-08-17 09:07:25 -0500

    Update admin.ctrl.js

--- public/app/routes/admin/admin.ctrl.js
+++ public/app/routes/admin/admin.ctrl.js
@@ -3,16 +3,22 @@ const SERVICE = new WeakMap();
 const TIMEOUT = new WeakMap();
 
 export class Controller {
+    
+    resetSearch() {
+        this.textSearch = '';
+        this.advancedFilters = {};
+        this.appliedAdvancedFilters = {};
+        this.appliedTextSearch = '';
+    }
 
 
-    constructor (currentUser, $timeout, companySvc, $scope) {
+    constructor (currentUser, $timeout, companySvc) {
 
         currentUser.whenReady().then(() => {
             this.user = currentUser.details;
         });
         this.companyActive = true;
         this.dataflowActive = false;
-
 
         SERVICE.set(this, companySvc);
         TIMEOUT.set(this, $timeout);
@@ -23,39 +29,54 @@ export class Controller {
         });
 
 
-        this.data = [{}];
-
-        this.searchDirty = false;
-        this.toggles = false;
-
-        this.supplierData = [
-            {name: "Supplier"},
-            {name: "Retailer"}
-        ];
-
-        this.products = [
-            {name: "Visibility"},
-            {name: "Order Performance"},
-            {name: "Compliance"}
-        ]
-
-        this.login = [
-            {name: "Last 24 Hours"},
-            {name: "Past Week"},
-            {name: "Last Month"},
-            {name: "Last 3 Months"},
-            {name: "Last 6 Months"}
-        ]
-
 
         INIT.get(this)();
+
+        //this.totalItems = 0;
+        //this.currentPage = 1;
+        //this.numPerPage = 10;
+
+        this.resetSearch();
+
+        this.searchDirty = false;
+        this.toggles = true;
+        this.showImage = true;
+
+        this.showData = function(){
+            this.curPage = 0;
+            this.pageSize = 3;
+            this.companies;
+            //this.datalists = [
+            //    { "name": "John","age":"16","designation":"Software Engineer1"},
+            //    {"name": "John2","age":"21","designation":"Software Engineer2"},
+            //    {"name": "John3","age":"19","designation":"Software Engineer3"},
+            //    {"name": "John4","age":"17","designation":"Software Engineer4"},
+            //    {"name": "John5","age":"21","designation":"Software Engineer5"},
+            //    {"name": "John6","age":"31","designation":"Software Engineer6"},
+            //    {"name": "John7","age":"41","designation":"Software Engineer7"},
+            //    {"name": "John8","age":"16","designation":"Software Engineer8"},
+            //    {"name": "John18","age":"16","designation":"Software Engineer9"},
+            //    {"name": "John28","age":"16","designation":"Software Engineer10"},
+            //    {"name": "John38","age":"16","designation":"Software Engineer11"},
+            //    {"name": "John48","age":"16","designation":"Software Engineer12"},
+            //    {"name": "John58","age":"16","designation":"Software Engineer13"},
+            //    {"name": "John68","age":"16","designation":"Software Engineer14"},
+            //    {"name": "John68","age":"16","designation":"Software Engineer15"}
+            //]
+            this.numberOfPages = function() {
+                return Math.ceil(this.companies.length / this.pageSize);
+            };
+
+        }
+
+
     }
 
 
-        // var companies = document.getElementById('companies');
+
+    // var companies = document.getElementById('companies');
         // console.log(companies.classList.contains('active'));
         // this.companyActive =  companies.classList.contains('active');
-
     setCompanyActive () {
         this.companyActive = true;
         this.dataflowActive = false;
@@ -69,56 +90,57 @@ export class Controller {
         this.dataflowActive = true;
     }
 
-    isChecked(id){
-    var match = false;
-    for(var i=0 ; i < this.data.length; i++) {
-        if($scope.data[i].id == id){
-            match = true;
-        }
+    setFilterValues(){
+        this.filterModel.retailerSelected = "retailer"
     }
-    return match;
-};
 
-    sync(bool, item){
-    if(bool){
-        // add item
-        this.data.push(item);
-    } else {
-        // remove item
-        for(var i=0 ; i < this.data.length; i++) {
-            if($scope.data[i].id == item.id){
-                $scope.data.splice(i,1);
-            }
+    applyFilter(){
+        this.searchDirty = true;
+        this.showImage = false;
+        this.theFilter = {
+            companyName : this.filterModel.companyName,
+            visibility : this.filterModel.visibility,
+            orderPerf : this.filterModel.orderPerf,
+            compliance : this.filterModel.compliance,
+            pos : this.filterModel.pos
+
         }
+
+        if(this.filterModel.visibility && this.filterModel.orderPerf && !this.filterModel.compliance){
+            this.theFilter.summary = "Visibility | Order Performance"
+        }else if(this.filterModel.visibility && this.filterModel.compliance && !this.filterModel.orderPerf){
+            this.theFilter.summary = "Visibility | Compliance"
+        }else if(this.filterModel.visibility && this.filterModel.orderPerf && this.filterModel.compliance){
+            this.theFilter.summary = "Visibility | Order Performance | Compliance"
+        }else if(this.filterModel.visibility && this.filterModel.orderPerf && this.filterModel.compliance && this.filterModel.pos){
+            this.theFilter.summary = "Visibility | Order Performance | Compliance | Point of Sale"
+        }else if(this.filterModel.orderPerf && this.filterModel.compliance && this.filterModel.pos){
+            this.theFilter.summary = "Order Performance | Compliance | Point of Sale"
+        }else if(!this.filterModel.visibility && this.filterModel.orderPerf && this.filterModel.compliance){
+            this.theFilter.summary = "Order Performance | Compliance"
+        }else if(this.filterModel.orderPerf && this.filterModel.pos){
+            this.theFilter.summary = "Order Performance | Point of Sale"
+        }else if(this.filterModel.compliance && this.filterModel.pos){
+            this.theFilter.summary = "Compliance | Point of Sale"
+        }else if(this.filterModel.visibility){
+            this.theFilter.summary = "Visibility"
+        }else if(this.filterModel.orderPerf) {
+            this.theFilter.summary = "Order Performance"
+        }else if(this.filterModel.compliance) {
+            this.theFilter.summary = "Compliance"
+        }else if(this.filterModel.pos) {
+            this.theFilter.summary = "Point of Sale"
+        }
+
+        if(this.filterModel.retailerSelected){
+            this.theFilter.retailerOrSupplier = "retailer";
+        }else if(this.filterModel.supplierSelected) {
+            this.theFilter.retailerOrSupplier = "supplier";
+        }
+
     }
-};
-    filterCompanies(){
-        return function (data) {
-            if ( $scope.search[data.retailerOrSupplier] === true ) {
-                return true;
-            }
-        }
-    };
 
-
-
-    //getResults() {
-    //    var companies = [];
-    //    this.$http.get("https://x8gqeax61e.execute-api.us-east-1.amazonaws.com/v0_0_4/company?name=&start&limit",
-    //        function (response) {
-    //            companies = response;
-    //        }, "json",
-    //        function (failure) {
-    //            console.log("failed :(", failure);
-    //        })
-    //
-    //    //this.companyName = companies.companyName;
-    //    //this.retailerOrSupplier = companies.retailerOrSupplier;
-    //    //this.capabilities = companies.capabilities;
-    //    //this.activeUsers = companies.activeUsers;
-    //}
 
 }
-Controller.$inject = ['currentUser', '$timeout', 'companySvc', '$scope']
-
+Controller.$inject = ['currentUser', '$timeout', 'companySvc']
 
